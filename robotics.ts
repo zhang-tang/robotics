ba9aa3d8d28a8d5fcc1becd64cf6a1742ced61d7
zhang-tang <80501060+zhang-tang@users.noreply.github.com>
2024-12-09 09:26:44 +0800

    MakeCode 项目的初始文件

---
+++ robotics.ts
@@ -0,0 +1,38 @@
+
+/**
+* 使用此文件来定义自定义函数和图形块。
+* 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace custom {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function foo(n: number, s: string, e: MyEnum): void {
+        // Add code here
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}
